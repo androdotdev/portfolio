a9f4da3bbc41a2cb9e7ae2224a8b427165af76d4
Rishabh Madhwal <121433064+androdotdev@users.noreply.github.com>
2026-03-06 22:32:43 +0530

    Add JsonFlow project information
    Add a new project 'JsonFlow' with details.

---
+++ data/projects.js
@@ -0,0 +1,10 @@
+const projects = [
+  {
+    name: "JsonFlow",
+    description: "Transforms JSON into interactive graphs.",
+    stack: ["Cytoscape", "Engine", "Graph"],
+    github: "https://github.com/androdotdev/json-flow",
+    demo: "https://json-flow-client.vercel.app/",
+    architecture: ""
+  },
+];
